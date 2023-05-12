cc1a7e9a833becfe9b9909e1501f1b57c95fa42d
Eduardo Aguirre <luis.24_aguirre@outlook.com>
2023-05-12 09:05:43 -0600

    first commit

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Product } from './../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private url = "https://young-sands-07814.herokuapp.com/api/products";
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllProducts() {
+    return this.http.get<Product[]>(this.url);
+  }
+
+  getProduct(id: string){
+    return this.http.get<Product>(`${this.url}/${id}`);
+  }
+}
