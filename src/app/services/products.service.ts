202c2bbf8be35d65fc10cca482ac210403c2cab0
Eduardo Aguirre <luis.24_aguirre@outlook.com>
2023-06-09 11:19:11 -0600

    refactor: :sparkles: add retry pipe with rxjs
    se agrego el pipe para reintentar solicitudes http con rxjs
    
    I-0002

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { createProductDTO, updateProductDTO ,Product } from './../models/product.model';
 
@@ -19,7 +20,8 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.url, { params });
+    return this.http.get<Product[]>(this.url, { params })
+    .pipe(retry(3));
   }
 
   getProductsByPage(limit: number, offset: number){
