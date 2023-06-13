5a17d4afe897a995e45bd9a4d69c19603783b124
Eduardo Aguirre <luis.24_aguirre@outlook.com>
2023-06-13 11:24:05 -0600

    feat: :sparkles: add pipe map in rxjs observable
    implementación del pipe map para transformar los datos desde front
    
    I-0004

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
-import { catchError, retry, throttleTime } from 'rxjs/operators';
+import { catchError, retry, map } from 'rxjs/operators';
 
 import { createProductDTO, updateProductDTO ,Product } from './../models/product.model';
 import { throwError } from 'rxjs';
@@ -22,13 +22,27 @@ export class ProductsService {
       params = params.set('offset', offset);
     }
     return this.http.get<Product[]>(this.url, { params })
-    .pipe(retry(3));
+    .pipe(
+      retry(3),
+       map(products => products.map(item => {
+          return {
+            ...item,
+            taxes: .18 * item.price
+          }
+       }))
+      );
   }
 
   getProductsByPage(limit: number, offset: number){
-    return this.http.get<Product[]>(this.url, {
-      params: {limit, offset}
-    });
+    return this.http.get<Product[]>(this.url, {params: {limit, offset}})
+    .pipe(
+      map(product => product.map(item => {
+        return {
+          ...item,
+          taxes: .18 * item.price
+        }
+      }))
+    );
   }
 
   getProduct(id: string){
